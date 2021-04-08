1dd24f895cef0777c2e3d5e199ad114317f77ed6
Hamid1828 <abdulkhamid1828@gmail.com>
2021-04-08 09:58:54 +0500

    portfolio done

---
+++ main.js
@@ -0,0 +1,42 @@
+$(".slick-slide").slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+$('.art-gallery').filterizr({});
+});
+
+function sidebar(){
+  document.querySelector('.art-info-bar').classList.toggle('show')
+}
